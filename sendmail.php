c1513f8012dd419ba1394bc347c00daef1da8d31
Sravan KSK <sravan.ksk0507@gmail.com>
2026-03-04 16:54:20 +0530

    Updated website content

---
+++ sendmail.php
@@ -0,0 +1,45 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // YOUR TEST EMAIL
+    $to = "sravan.ksk0507@gmail.com";
+
+    $email_subject = "Website Test Inquiry: $subject";
+
+    $email_body = "
+New website message:
+
+Name: $name
+Email: $email
+Phone: $phone
+Subject: $subject
+
+Message:
+$message
+";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if(mail($to, $email_subject, $email_body, $headers)) {
+        echo "<script>
+        alert('Message sent successfully!');
+        window.location.href='contact.html';
+        </script>";
+    } else {
+        echo "<script>
+        alert('Message failed to send.');
+        window.location.href='contact.html';
+        </script>";
+    }
+
+}
+
+?>
